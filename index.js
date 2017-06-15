b09e277ba0ee4fbfd266a1ba5b4de2b76aff8726
Daniel Catzva <Daniel_Catzva@discovery.com>
2017-06-15 17:01:08 -0400

    switched to Promise all for fetching

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 
 document.addEventListener("DOMContentLoaded", function(event) { 
-  console.log("loaded!")
-  
-  //var form = document.querySelector("form");
-  //console.log(form);
 
   document.getElementById("submitbutton").onclick = function(event){ 
     event.preventDefault();
     var subred = document.getElementById("subreddit").value;
-    console.log('did stuff #1', subred); 
-
     var today = new Date();
-    var yago = new Date(today.setMonth(today.getMonth()-12));
+    var yago = new Date(new Date(today).setMonth(today.getMonth()-12));
     yago.setDate(1);
+
+    //iterate through all start/stop times
+    var start, stop, monthdates;
+    stop = new Date(today);
+    start = new Date(today.setDate(1));
+    monthdates = [];
+    months = 11
+
+    while(months>=0){
+      monthdates.push([start.getTime(), stop.getTime()]);
+      start = new Date(new Date(start).setMonth(start.getMonth()-1));
+      stop = new Date(new Date(new Date(start).setMonth(start.getMonth()+1)).setDate(0));
+      months--;
+    }
+
+    //for each month - make an API call
+    var baseurl = "https://www.reddit.com/r/news/top/.json?count=20&timestamp:";
+ 
+    Promise.all(monthdates.map((dateset)=>{
+        url = baseurl + dateset[0]+".."+dateset[1]
+        return fetch(url).then(response => response.json())
+    })).then(val => {
+        console.log("ALL LOADED!!!", val);
+    })
+
+
+    //calc monthy avgs and other decriptive stats
+
+    //extract timeline data - one array of data that includes: 
+    //num_comments, score,  title, url, ups, downs
     
 
-    console.log(today.getTime(), yago.getTime())
-
-    fetch('https://www.reddit.com/r/news/top/.json?count=20&timestamp:1418515200..1418601600').then(function(response) {
-      return response.json();
-    }).then(function(dat) {
-      console.log(dat.data.children);
-    });
+    
 
   }
 
