edfc3d9b137b9d2131dce3300dd5ee658e081773
Dan Catzva <dcatzva@gmail.com>
2017-06-19 08:43:33 -0400

    got the reddit url to work

--- index.js
+++ index.js
@@ -1,46 +1,68 @@
 
-document.addEventListener("DOMContentLoaded", function(event) { 
+document.addEventListener("DOMContentLoaded", function(event) {
+  var articleCount=20;
+  var selectedYear=2017;
+  this.getElementById("articleCountIndicator").innerHTML = articleCount;
+  var start, stop;
 
   document.getElementById("submitbutton").onclick = function(event){ 
     event.preventDefault();
-    var subred = document.getElementById("subreddit").value;
-    var today = new Date();
-    var yago = new Date(new Date(today).setMonth(today.getMonth()-12));
-    yago.setDate(1);
+    //var subred = document.getElementById("subreddit").value;
+    //var today = new Date();
+    //var yago = new Date(new Date(today).setMonth(today.getMonth()-12));
+    //yago.setDate(1);
 
-    //iterate through all start/stop times
-    var start, stop, monthdates;
-    stop = new Date(today);
-    start = new Date(today.setDate(1));
-    monthdates = [];
-    months = 11
+    var monthdates, url, months, baseurl, rawdata, timelineData;
+    //stop = new Date(today);
+    //start = new Date(today.setDate(1));
+    //monthdates = [];
+    //months = 11
+    baseurl = "https://www.reddit.com/r/news/search.json?sort=top";
+    url = baseurl + "&limit="+articleCount+"&q=timestamp%3A";
+    url += (Math.trunc(start.getTime()/1000)) +".."+(Math.trunc(stop.getTime()/1000))+"&restrict_sr=on&syntax=cloudsearch";
+    console.log(url)
 
-    while(months>=0){
-      monthdates.push([start.getTime(), stop.getTime()]);
-      start = new Date(new Date(start).setMonth(start.getMonth()-1));
-      stop = new Date(new Date(new Date(start).setMonth(start.getMonth()+1)).setDate(0));
-      months--;
-    }
+    var p = new Promise((resolve,reject)=>{
+      fetch(url).then(response => resolve(response.json()))
 
-    //for each month - make an API call
-    var baseurl = "https://www.reddit.com/r/news/top/.json?count=20&timestamp:";
- 
-    Promise.all(monthdates.map((dateset)=>{
-        url = baseurl + dateset[0]+".."+dateset[1]
-        return fetch(url).then(response => response.json())
-    })).then(val => {
-        console.log("ALL LOADED!!!", val);
-    })
+    }).then(val => {
+        rawdata = (val.data.children).map((v)=>{return {"url": v.data["url"], "score": v.data["score"], "ups": v.data["ups"], "date": v.data["created_utc"], "downs": v.data["downs"], "title": v.data["title"], "comments": v.data["num_comments"]}});
+        console.log("ALL LOADED!!!", rawdata);
+        });    
+  }
 
-
-    //calc monthy avgs and other decriptive stats
-
-    //extract timeline data - one array of data that includes: 
-    //num_comments, score,  title, url, ups, downs
-    
-
-    
+  var yearButtonClick=(e)=>{
+    e.preventDefault();
+    console.log("button clicked", e.target.value)
+    selectedYear=e.target.value;
+    start = new Date(selectedYear, 0, 1);
+    stop = new Date(selectedYear, 12, 0);
+    if(stop>(new Date())){ stop=new Date()};
+    console.log(start, stop);
 
   }
 
-});
+  var articleCountButtonClick=(e)=>{
+    e.preventDefault();
+    console.log("number of articles changed");
+    
+    if(e.target.value=="plus"){
+      if(articleCount<100){articleCount++;}
+    }
+    else{
+      if(articleCount>0){articleCount--;}
+    }
+  
+    this.getElementById("articleCountIndicator").innerHTML = articleCount;
+
+  }
+
+  var ys = document.getElementsByClassName("yearselect");
+  ys = Array.prototype.slice.call(ys);
+  ys.map((e)=>{e.onclick=yearButtonClick});
+
+  var ac = document.getElementsByClassName("articleCount");
+  ac = Array.prototype.slice.call(ac);
+  ac.map((e)=>{e.onclick=articleCountButtonClick});
+
+  });
