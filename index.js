45361b524c986724df6b6415205c3442be5c3a41
Dan Catzva <dcatzva@gmail.com>
2017-06-24 01:33:05 -0400

    lots of formattig updates.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
   let selectedYear = 2017;
   this.getElementById("articleCountIndicator").innerHTML = articleCount;
   let start, stop;
-  let subreddits = ["news", "politics", "worldnews", "television", "politics"];
+  let subreddits = ["news", "politics", "worldnews", "television", "science"];
   //let subreddits = ["news"];
   let rawdata = [];
 
@@ -17,12 +17,21 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
     let p = Promise.all(subreddits.map(sub => {
       url = baseurl + sub + endurl;
-      return fetch(url).then(response => response.json().then(dat => { return dat.data.children.map(art => { return { "url": art.data["url"], "score": art.data["score"], "ups": art.data["ups"], "date": art.data["created_utc"], "downs": art.data["downs"], "title": art.data["title"], "comments": art.data["num_comments"] } }) }))
+      return fetch(url).then(response => response.json().then(dat => 
+                        { return dat.data.children.map(art => 
+                          { return { "url": art.data["url"], 
+                                     "score": art.data["score"], 
+                                     "ups": art.data["ups"], 
+                                     "date": art.data["created_utc"], 
+                                     "downs": art.data["downs"], 
+                                     "title": art.data["title"], 
+                                     "comments": art.data["num_comments"] } }) }))
     })).then(val => {
       console.log("ALL LOADED!!!", document.getElementById("svg-header").width);
       document.getElementById("svg-main").innerHTML = "";
+      document.getElementById("svg-header").innerHTML = "";
 
-      drawTimelines(val[0], document.getElementById("svg-main"), [start, stop]);
+      drawTimelines(document.getElementById("svg-main"), [start, stop], val);
       drawScaleLines(document.getElementById("svg-header"), [start, stop])
     })
   }
@@ -30,7 +39,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   let updateYearSelected = function (yr) {
     selectedYear = parseInt(yr);
-    let ys = document.getElementsByClassName("yearselect");//.classList.add("greyout");
+    let ys = document.getElementsByClassName("yearselect");
     ys = Array.prototype.slice.call(ys);
     ys.forEach(function (element) {
       if (element.value != selectedYear) {
@@ -44,9 +53,7 @@ document.addEventListener("DOMContentLoaded", function (event) {
     start = new Date(selectedYear, 0, 1);
     stop = new Date(selectedYear, 12, 0);
     if (stop > (new Date())) { stop = new Date() };
-    console.log(start, stop);
   }
-
 
   let yearButtonClick = (e) => {
     e.preventDefault();
@@ -55,7 +62,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
 
   let articleCountButtonClick = (e) => {
     e.preventDefault();
-    console.log("number of articles changed");
 
     if (e.target.value == "plus") {
       if (articleCount < 100) { articleCount++; }
@@ -65,7 +71,6 @@ document.addEventListener("DOMContentLoaded", function (event) {
     }
 
     this.getElementById("articleCountIndicator").innerHTML = articleCount;
-
   }
 
   let ys = document.getElementsByClassName("yearselect");
@@ -76,112 +81,118 @@ document.addEventListener("DOMContentLoaded", function (event) {
   ac = Array.prototype.slice.call(ac);
   ac.map((e) => { e.onclick = articleCountButtonClick });
 
-
   updateYearSelected(selectedYear);
 
-
   let drawScaleLines = function (svgTarget, dates) {
-      let svg = d3.select(svgTarget).append("svg"),
-          width = d3.select(svgTarget).node().getBoundingClientRect().width;
-          console.log(width);
-      svg.attr("width", width);
+    let svg = d3.select(svgTarget).append("svg"),
+      width = d3.select(svgTarget).node().getBoundingClientRect().width,
+      height = d3.select(svgTarget).node().getBoundingClientRect().height;
 
-      var x = d3.scaleTime()
-        .domain([new Date(dates[0]), new Date(dates[1])])
-        .range([0, width]);
+    svg.attr("width", width);
 
-      var xAxis = d3.axisBottom(x)
-          .ticks(d3.timeMonths(new Date(dates[0]), new Date(dates[1])).range)
-          .tickSize(16, 0)
-          .tickFormat(d3.timeFormat("%B"));
+    var x = d3.scaleTime()
+      .domain([new Date(dates[0]), new Date(dates[1])])
+      .range([0, width]);
 
-      svg.append("g")
-        .attr("class", "x axis")
-        .attr("transform", "translate(0,0)")
-        .call(xAxis)
-      .selectAll(".tick text")
-        .style("text-anchor", "start")
-        .attr("x", 6)
-        .attr("y", 6);
-      /*
-        let xlabels = []
-        let monthDiff = function(d1, d2) {
-            var months;
-            months = (d2.getFullYear() - d1.getFullYear()) * 12;
-            months -= d1.getMonth() + 1;
-            months += d2.getMonth();
-            return months <= 0 ? 0 : months;
-        }
+    var xAxis = d3.axisBottom(x)
+      .ticks(d3.timeMonths(new Date(dates[0]), new Date(dates[1])).range)
+      .tickSize(12, 0)
+      .tickFormat(d3.timeFormat("%B"));
 
-        for (var index = 0; index < monthDiff(new Date(dates[0]), new Date(dates[1])); index++) {
-          xlabels.push(startYear + index);
-        }
-    
-        var xAxis = d3.axisBottom(x);
-        xAxis.ticks(10);
-        xAxis.tickFormat(function (d, i) {
-          if (i % 4 == 0) return xlabels[i];
-          return "";
-        })
-        // Add the x-axis.
-        svg.selectAll(".xaxis").remove()
-        svg.append("g")
-          .attr("class", "xaxis")
-          .attr("transform", "translate(0," + (height - MARGIN) + ")")
-          .call(xAxis);
+    var bubbleGroup = svg.append("g")
+      .attr("class", "x axis")
+      .style("font-family", "mainfont")
+      .attr("transform", "translate(0," + (height - 20) + ")")
+      .call(xAxis);
 
-          */
-    
-          
+    bubbleGroup.selectAll(".tick text")
+      .style("text-anchor", "start")
+      .attr("x", 6)
+      .attr("y", 6);
   }
 
-  let drawTimelines = function (data, svgTarget, dates) {
-    console.log("drawing the timelines");
+  let drawTimelines = function (svgTarget, dates, dataSets) {
+
     //Sort data by ups - largest to smallest - so when drawing the smaller ones will appear on top
 
     let formatDate = d3.timeFormat("%d-%b-%y");
     let svg = d3.select(svgTarget).append("svg");
-    let maxval = Math.max.apply(null, data.map(d=>{return d["ups"]}));
-    //console.log(maxval);
-
-    
-    
     let margin = { top: 20, right: 30, bottom: 40, left: 0 };
     let height = 100,
       MARGIN = 10,
-      MAXBALLOON_SIZE = 30;
+      MAXBALLOON_SIZE = 30,
+      ROW_GAP = 100,
+      ypos = 100;
 
     let containerW = parseInt((window.getComputedStyle(svgTarget).width).replace("px", "")),
       containerH = parseInt((window.getComputedStyle(svgTarget).height).replace("px", ""));
 
+    svg.attr("class", "svg");
     svg.attr("width", containerW)
       .attr("height", containerH);
 
-
     var x = d3.scaleLinear()
-      .domain([start.getTime()/1000, stop.getTime()/1000])
+      .domain([start.getTime() / 1000, stop.getTime() / 1000])
       .range([10, (containerW - margin.left - margin.right)])
       .interpolate(d3.interpolateRound)
 
     svg.selectAll(".bubbles").remove()
-    var bubble = svg.selectAll(".bubbles")
-      .data(data)
-      .enter().append("g")
-      .attr("transform", function (d, i) {return "translate(" + x(new Date(d["date"])) + ",100)"; });
-    
-    bubble.append("circle")
-      .attr("class", "bub")
-      .attr("r", (d) => {return (d["ups"]/maxval) * MAXBALLOON_SIZE });
+    var bubble, maxval, textbubble;
 
-    bubble.append("text")
-      .attr("y", -40)
-      .attr("x", 0)
-      .attr("text-anchor", "middle")
-      .text((d) => {
-        let date = new Date(d["date"]*1000);
-        return ((date.getMonth()+1) +"/"+ (date.getDate()+1));
+    dataSets.forEach((data, setnum) => {
+      //each set will have its own scale!
+      maxval = Math.max.apply(null, data.map(d => { console.log(); return d["ups"] }));
+
+      //draw the horizontal axis for each data set
+      svg.append("line")
+        .attr("class", "horiz-line")
+        .attr("x1", 0)
+        .attr("y1", (ypos + ROW_GAP * setnum))
+        .attr("x2", containerW)
+        .attr("y2", (ypos + ROW_GAP * setnum));
+
+      //add a label for the subreddit
+      svg.append("foreignObject")
+        .attr("y", (ypos + ROW_GAP * setnum)-45)
+        .attr("x", 8)
+        .attr("text-anchor", "left")
+        .html((d,i) => {
+          return ("<span class='subreddit-label'>/"+subreddits[setnum]+":</span>");
         });
 
-  
+      let onRollOver = function (d, i) {
+        let t = d3.select(this.parentNode)
+        t.selectAll("g > .textbubble").attr("class", "textbubble show");
+      }
+
+      let onRollOff = function (d, i) {
+        let t = d3.select(this.parentNode)
+        t.select("g > .textbubble").attr("class", "textbubble hide");
+      }
+
+      bubble = svg.selectAll(".bubbles")
+        .data(data)
+        .enter().append("g")
+        .attr("transform", function (d, i) { return "translate(" + x(new Date(d["date"])) + "," + (ypos + ROW_GAP * setnum) + ")"; });
+
+      bubble.append("circle")
+        .attr("class", "bub")
+        .attr("r", (d) => { return (d["ups"] / maxval) * MAXBALLOON_SIZE })
+        .on("mouseover", onRollOver)
+        .on("mouseout", onRollOff);
+
+      bubble.append("foreignObject")
+        .attr("class", "textbubble hide")
+        .attr("y", -110)
+        .attr("x", -75)
+        .attr("text-anchor", "middle")
+        .html((d) => {
+          let date = new Date(d["date"] * 1000);
+          return ("<div class='bubtext'>(" + (date.getMonth() + 1) + "/" + (date.getDate() + 1) + ") " + d["title"] + "</div>");
+        });
+    });
+
+
+
   }
 });
